7cc77b6f57cf3dbfe6790d33d3be2cabe48505f1
apuu03 <57383340+apuu03@users.noreply.github.com>
2025-06-11 11:38:11 -0300

    feat: mejorar manejo de solicitudes en server-db.js y agregar validación de conexión a la base de datos

--- caso-01/server-db.js
+++ caso-01/server-db.js
@@ -1,12 +1,20 @@
 import http from 'http'
+import { FormatRes } from './utils/formatRes.js'
+import { getConnection } from './routesDb/config/db.js'
 import { getRoutes } from './routes/get.js'
+import { deleteRoutes } from './routes/delete.js'
 import { postRoutes } from './routes/post.js'
 import { putRoutes } from './routes/put.js'
-import { deleteRoutes } from './routes/delete.js'
-
+import { patchRoutes } from './routes/patch.js'
 const desiredPort = 3000
 
-function requestHandler (req, res) {
+async function requestHandler (req, res) {
+  const response = new FormatRes(res)
+  // Validar si hay db antes de continuar
+  const check = await getConnection()
+  if (check?.abort) {
+    return response.serverUnavailable('Base de datos caída o no conectada')
+  }
   if (req.method === 'GET') {
     getRoutes(req, res)
   } else if (req.method === 'POST') {
@@ -15,6 +23,10 @@ function requestHandler (req, res) {
     putRoutes(req, res)
   } else if (req.method === 'DELETE') {
     deleteRoutes(req, res)
+  } else if (req.method === 'PATCH') {
+    patchRoutes(req, res)
+  } else {
+    return response.forbidden()
   }
 }
 
