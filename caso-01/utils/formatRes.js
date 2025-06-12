b5bb5b3c94eaf3984ac318cadc194ea81f567cd9
AbrilSabatini <abrilsabatini2004@gmail.com>
2025-06-11 23:26:47 -0300

    update: added unauthorized response

--- caso-01/utils/formatRes.js
+++ caso-01/utils/formatRes.js
@@ -1,76 +1,80 @@
 export class FormatRes {
-  constructor (res) {
+  constructor(res) {
     this.res = res
   }
 
   // creo el send que es la base de las demás funciones de respuesta
-  send (status, data, headers = { 'Content-Type': 'application/json; charset=utf-8' }) {
+  send(status, data, headers = { 'Content-Type': 'application/json; charset=utf-8' }) {
     this.res.writeHead(status, headers)
     return this.res.end(JSON.stringify(data))
   }
 
   // success envía a send como status 200 y como data = data
-  success (data) {
+  success(data) {
     return this.send(200, data)
   }
 
-  created (data) {
+  created(data) {
     return this.send(201, data)
   }
 
-  accepted (data) {
+  accepted(data) {
     return this.send(202, data)
   }
 
-  noContent (message = 'Cuerpo de la solicitud vacío') {
+  noContent(message = 'Cuerpo de la solicitud vacío') {
     return this.send(204, { error: message })
   }
 
-  badRequest (message = 'Solicitud incorrecta') {
+  badRequest(message = 'Solicitud incorrecta') {
     return this.send(400, { error: message })
   }
 
-  forbidden (message = 'Método no permitido') {
-    return this.send(403, { error: message })
+  unauthorized(message = "Acceso no autorizado") {
+    return this.send(401, { error: message });
   }
 
-  notFound (message = 'No encontrado') {
+  forbidden(message = "Acceso prohibido") {
+    return this.send(403, { error: message });
+  }
+
+  notFound(message = 'No encontrado') {
     return this.send(404, { error: message })
   }
 
-  notFoundRoute () {
+  notFoundRoute() {
     return this.send(404, { error: 'Ruta no encontrada' })
   }
 
-  notAcceptable (message = 'Datos incorrectos') {
+  notAcceptable(message = 'Datos incorrectos') {
     return this.send(406, { error: message })
   }
 
-  conflict (message = 'No se modificó el elemento') {
+  conflict(message = 'No se modificó el elemento') {
     return this.send(409, { error: message })
   }
 
-  lengthRequired (message = 'Se requieren mínimo 5 caracteres') {
+  lengthRequired(message = 'Se requieren mínimo 5 caracteres') {
     return this.send(411, { error: message })
   }
 
-  urlTooLong (message = 'La url es demasiado larga') {
+  urlTooLong(message = 'La url es demasiado larga') {
     return this.send(414, { error: message })
   }
 
-  unsupportedMediaType (message = 'No es un JSON valido') {
+  unsupportedMediaType(message = 'No es un JSON valido') {
     return this.send(415, { error: message })
   }
 
-  serverError (message = 'Error interno del servidor') {
+  serverError(message = 'Error interno del servidor') {
     return this.send(500, { error: message })
   }
 
-  notImplemented (message = 'Funcionalidad no implementada') {
+  notImplemented(message = 'Funcionalidad no implementada') {
     return this.send(501, { error: message })
   }
 
-  serverUnavailable (message = 'Servidor caído') {
+  serverUnavailable(message = 'Servidor caído') {
     return this.send(503, { error: message })
   }
 }
